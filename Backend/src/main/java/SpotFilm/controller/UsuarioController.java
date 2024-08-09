582606b694184c6210eeb8504f38d4db3e8a9d70
Thaissa Lopes <117691038+thatalps@users.noreply.github.com>
2024-08-09 20:45:22 -0300

    ajustes para uso de criptografia e correção de endpoint

--- Backend/src/main/java/SpotFilm/controller/UsuarioController.java
+++ Backend/src/main/java/SpotFilm/controller/UsuarioController.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api")
@@ -17,15 +20,15 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-    BCryptPasswordEncoder criptografar = new BCryptPasswordEncoder();
+
+    private BCryptPasswordEncoder criptografar;
 
     private static final Logger logger = LoggerFactory.getLogger(UsuarioRepository.class);
 
     @PostMapping("/usuarios/cadastro")
     public void cadastro(@RequestBody Usuario usuario)
     {
-        String senhaCriptografada = criptografar.encode(usuario.getSenha());
-        usuario.setSenha(senhaCriptografada);
+        usuario.setSenha(criptografar.encode(usuario.getSenha()));
         usuarioRepository.save(usuario);
     }
 
