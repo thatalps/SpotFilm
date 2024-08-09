9ef8d7924f1c662b0b5bee2897351e54b0a79996
Mylene <myleneferreira4@edu.unirio.com.br>
2024-08-09 19:37:13 -0300

    Correção classe Genero

--- Backend/src/main/java/SpotFilm/model/Genero.java
+++ Backend/src/main/java/SpotFilm/model/Genero.java
@@ -1,32 +1,17 @@
 package SpotFilm.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
 public class Genero {
+    @Getter
+    @Setter
+    @JsonProperty("id")
     private int id;
+
+    @Getter
+    @Setter
+    @JsonProperty("name")
     private String name;
-
-    // Construtor padrão
-    public Genero() {
-    }
-
-    // Construtor com argumentos (opcional)
-    public Genero(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    // Getters e setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    // toString() opcional para facilitar a depuração
-    @Override
-    public String toString() {
-        return "Genero{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
