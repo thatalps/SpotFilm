7cbea77ce02e34be450a1df78bd1fffd4ac32380
Thaissa Lopes <117691038+thatalps@users.noreply.github.com>
2024-08-25 14:01:28 -0300

    ajuste para remover avaliacao do comentario

--- Backend/src/main/java/SpotFilm/controller/ComentarioController.java
+++ Backend/src/main/java/SpotFilm/controller/ComentarioController.java
@@ -1,28 +1,22 @@
 package SpotFilm.controller;
 
-import SpotFilm.dto.ApiResposta;
-import SpotFilm.dto.ComentarioResposta;
-import SpotFilm.model.Avaliacao;
 import SpotFilm.model.Comentario;
-import SpotFilm.repository.AvaliacaoRepository;
 import SpotFilm.repository.ComentarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/cometario")
+@RequestMapping("/comentario")
 public class ComentarioController {
 
     @Autowired
     ComentarioRepository comentarioRepository;
-    AvaliacaoRepository avaliacaoRepository;
 
     @PostMapping("post")
     public ResponseEntity<String> addComentario(@RequestBody Comentario comentario) {
--- Backend/src/main/java/SpotFilm/repository/ComentarioRepository.java
+++ Backend/src/main/java/SpotFilm/repository/ComentarioRepository.java
@@ -4,7 +4,6 @@ import SpotFilm.model.Comentario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ComentarioRepository extends MongoRepository<Comentario, String> {
