f7efa5ac66cec922e0e9b280c6afa4860fc19ebd
Mylene <myleneferreira4@edu.unirio.com.br>
2024-08-26 19:06:37 -0300

    Recomendação por genero

--- Backend/src/main/java/SpotFilm/controller/UsuarioController.java
+++ Backend/src/main/java/SpotFilm/controller/UsuarioController.java
@@ -8,7 +8,6 @@ import SpotFilm.model.Usuario;
 import SpotFilm.repository.UsuarioRepository;
 import SpotFilm.service.ApiService;
 import SpotFilm.util.Autenticador;
-import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
